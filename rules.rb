7edc35bd37022181cb65d70fd13094b1f183af39
codebully <bookthesevens@gmail.com>
2014-07-15 20:51:43 -0400

    refactored how arrows calculate

--- rules.rb
+++ rules.rb
@@ -1,15 +1,16 @@
 def translate_arrows
   $intersection_choice = $intersection_choice.downcase
   case
-    when $intersection_choice == 'left'
+    when $intersection_choice.start_with?('l')
       $intersection_choice = 'l'
-    when $intersection_choice == 'right'
+    when $intersection_choice.start_with?('r')
       $intersection_choice = 'r'
-    when $intersection_choice == 'up'
+    when $intersection_choice.start_with?('u')
       $intersection_choice = 'u'
-    when $intersection_choice == 'down'
+    when $intersection_choice.start_with?('d')
       $intersection_choice = 'd'
     else
       $intersection_choice = nil
+
   end
 end
