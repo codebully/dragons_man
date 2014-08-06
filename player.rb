85d4f6983a67e1f6a736d757d11b848c9a9edabe
Nikita Hasis <nhasis@ha003249.wvrgroup.internal>
2014-08-06 16:18:36 -0500

    fixed?

--- player.rb
+++ player.rb
@@ -1,27 +1,4 @@
-<<<<<<< HEAD
-def new_player
-	$player = { :player_name => "", 
-				:class => "", 
-				:health => 100, 
-				:str => 10, 
-				:xp => 1, 
-				:cash => 0,
-				:inventory => @inventory = []
-				}
 
-	prompt; $player[:name] = gets.chomp
-
-	if $player[:name].downcase == "bob"
-		$player = { 	:name => "Bob", 
-					:class => "God", 
-					:health => 200, 
-					:str => 20,
-					:xp => 1,
-					:cash => 0
-					}
-	else
-		$player[:player_class] = "punter"
-=======
 class Player
 	def initialize
 		puts "What's your name, #{$insults.sample}?"
@@ -30,60 +7,10 @@ class Player
 		@str = 10
 		@xp = 0
 		@cash = 0
->>>>>>> classes
 	end
 	attr_accessor :name, :health, :str, :xp, :cash
 end
 
-<<<<<<< HEAD
-# method to call random monster
-# array of monsters
-# each monster in hash
-# then pop a sample out of array.
-# TODO: Add another layer when monsters are grouped by class. 
-# dragons w/ dragons, etc. this way you can pull out a dragon when you need to.
-# TODO: randomize attributes within a range. 
-# TODO: randomize names.
-
-def ran_monster
-
-	monster = [
-
-		dragon = {
-			:name => @names.sample,
-			:class => 'Dragon',
-			:health => 10 + rand(40),
-			:str => 1 + rand(5),
-			:cash => rand(10)
-		},
-
-		midget = {
-			:name => @names.sample,
-			:class => 'Angry Midget',
-			:health => 10 + rand(30),
-			:str => 1 + rand(3),
-			:cash => rand(7)
-		},
-
-		pigeon = {
-			:name => @names.sample,
-			:class => 'pigeon',
-			:health => 1 + rand(6),
-			:str => 1 + rand(2),
-			:cash => rand(2)
-		},
-
-		zombie = {
-			:name => @names.sample,
-			:class => 'zombie',
-			:health => 1 + rand(25),
-			:str => 1 + rand(4),
-			:cash => rand(6)
-		}
-	]
-
-	@monster = monster.sample
-=======
 class Dragon
 	def initialize 
 		@name = $names.sample
@@ -92,5 +19,4 @@ class Dragon
 		@cash = 1 + rand(10)
 	end
 	attr_accessor :name, :health, :str, :cash
->>>>>>> classes
 end
